8d309efc19fa6c78cae9891a29d10989681a1b7b
gstoll9 <70171992+gstoll9@users.noreply.github.com>
2025-05-21 18:50:21 -0400

    Create hubbardModel.tsx

---
+++ src/app/hubbardModel.tsx
@@ -0,0 +1,14 @@
+function HubbardModelPage() {
+    return (<>
+        <h1>Hubbard Model</h1>
+        <div className="divider"/>
+        <p></p>
+        {/*plot cobtrols*/}
+        {/*plot*/}
+        <h2>Conclusion and Further Research</h2>
+        <div className="divider"/>
+        <p></p>
+    </>)
+}
+
+export default HubbardModelPage
